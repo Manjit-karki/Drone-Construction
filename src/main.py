74b58bf88176acce195fd432585563195097144f
sire-k <ash.yakkha@gmail.com>
2025-12-21 07:54:06 +0545

    Some additions...?

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+from utils.camera_status import camera_status
+
+def main():
+    print("Starting Drone Construction Project...")
+    status = camera_status()
+    print(status)
+    
+if __name__ == "__main__":
+    main()
